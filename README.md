bd8c345eaac2123f847829d280f46c1d9aaee648
Andrew <gitkraken@hinesystems.com>
2022-04-11 19:01:10 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+PGP JS web page and chrome extension right click process and replace textarea selected text source
---
+++ keys/keys.js
@@ -0,0 +1,39 @@
+// Generated from https://www.igolder.com/pgp/generate-key/
+
+// Must contain line breaks within key and at end and last \ is for multi-line JS comment
+// Must NOT start with a line break
+// 2nd and 3rd character from my password have been removed after SAfZFo - replaced in PGDecode.js when PW entered
+var privateKey = '-----BEGIN PGP PRIVATE KEY BLOCK-----\n\
+Version: BCPG C# v1.6.1.0\n\
+\n\
+lQHsBE6xcHQBBACyU5IWzYcaxKRNi6sAtcYI7l+kaawtF18ttuI63affeUXl2Wq1\n\
+0JHV+H8FYu1iSmqP59qCAL+T3i3MdZm5ZsTJTVeKqav6pSb0L2gnokPmroDBrOwH\n\
+o4717mKuFjMolzHnCNvIWVjldTjvBdVqgLYrc4zFH24w4CdTvr23iajSyQARAQAB\n\
+/wMDAl8KVbQRidMJYCo96BBPCsZKfGdPvc5xx3Pg4VnMak1/4f6epuyaVFNsHQkE\n\
+Zm+WwXM+6mG3MQeAAMtSvnWw6UVg/DWG4NVBY4Acftf3d1k6PHK4AlS78or4g5Ym\n\
++D2+mzjSAfZFoYno0C+QZgaJA6J8d+wehAzO3rFnDCbl0fQCZrxL1SWQvhzE0c\n\
+4ArEiLgjOYCNTWyfTYpjLbIwdwLpKJpzwcrj6nCU3OxRQJclMpRm8JEg54kmAD/h\n\
+pS534NVG6i0Gbq6SmkyU2O05kjfDdkuUmwziSHNWUY2w88Db7sNDP25sWpF0t8os\n\
+nv3oALokF9NEwPqQbwI9/8/RkUkMYYTAIRWRQ9LMoxhupY19JHdQKAmE+VxhcWLs\n\
+BFZ9NLrPpWnMPhCuZVwCuvTMDXNhUOa7dcIX+XBtgp+HHQ9NBpnh9ze4kTlWKOV8\n\
+rFCqWoqfCDKZhYHfEDQstA9wZ3BAaGluZS5vcmcudWuInAQQAQIABgUCTrFwdAAK\n\
+CRBgXWHFFkf5awg5A/9c6Jt62TpdLZcaNe4jkuTTsph99T9lc8y786L3ZL337Gyv\n\
+fVVCM+h4CzDEiIjPj6LwhCicacu9IJA/RuNSKoRB+ZEzma2BAoNaYLUPeBO8ml2c\n\
+eJUOS1WH5TQrXo2GHhd/jmV+MCk/n6gEcLsxrmii+MLCvwP8ezH/7HV2umh2wA==\n\
+=j8Ux\n\
+-----END PGP PRIVATE KEY BLOCK-----\n\
+';
+
+var publicKey = '-----BEGIN PGP PUBLIC KEY BLOCK-----\n\
+Version: BCPG C# v1.6.1.0\n\
+\n\
+mI0ETrFwdAEEALJTkhbNhxrEpE2LqwC1xgjuX6RprC0XXy224jrdp995ReXZarXQ\n\
+kdX4fwVi7WJKao/n2oIAv5PeLcx1mblmxMlNV4qpq/qlJvQvaCeiQ+augMGs7Aej\n\
+jvXuYq4WMyiXMecI28hZWOV1OO8F1WqAtitzjMUfbjDgJ1O+vbeJqNLJABEBAAG0\n\
+D3BncEBoaW5lLm9yZy51a4icBBABAgAGBQJOsXB0AAoJEGBdYcUWR/lrCDkD/1zo\n\
+m3rZOl0tlxo17iOS5NOymH31P2VzzLvzovdkvffsbK99VUIz6HgLMMSIiM+PovCE\n\
+KJxpy70gkD9G41IqhEH5kTOZrYECg1pgtQ94E7yaXZx4lQ5LVYflNCtejYYeF3+O\n\
+ZX4wKT+fqARwuzGuaKL4wsK/A/x7Mf/sdXa6aHbA\n\
+=eop0\n\
+-----END PGP PUBLIC KEY BLOCK-----\n\
+';
